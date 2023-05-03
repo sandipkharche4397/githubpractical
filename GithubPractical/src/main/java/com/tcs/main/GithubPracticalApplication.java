53e77dd7c55a64d0abe5664fd36cc39c68eb5638
sandipkharche4397 <sandipkharche4397@gmail.com>
2023-05-03 15:21:58 +0530

    first commit

---
+++ GithubPractical/src/main/java/com/tcs/main/GithubPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubPracticalApplication.class, args);
+	}
+
+}
